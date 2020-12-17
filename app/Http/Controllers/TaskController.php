dd10f47797d485ad264f6b774a413df86c78ad93
林駿綦 <r10101720307@gmail.com>
2020-12-17 23:55:50 +0800

    編輯 TaskController 的 destroy 方法：刪除任務（有授權）

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -105,8 +105,12 @@ class TaskController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, Task $task)
     {
-        //
+        $this->authorize('destroy', $task);
+
+        $task->delete();
+
+        return redirect('/tasks');
     }
 }
